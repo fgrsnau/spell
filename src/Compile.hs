d8ad584a35058e4a5871dcd222cf9b02a773193f
Stefan Haller <stefan.haller@tu-dresden.de>
2014-06-17 23:32:31 +0200

    Adds documentation in Main modules.

--- src/Compile.hs
+++ src/Compile.hs
@@ -1,3 +1,23 @@
+{-# LANGUAGE Trustworthy #-}
+
+-- |
+-- Module      : Main
+-- Description : Compiler for wordlists.
+-- Copyright   : (c) Stefan Haller, 2014
+--
+-- License     : MIT
+-- Maintainer  : s6171690@mail.zih.tu-dresden.de
+-- Stability   : experimental
+-- Portability : portable
+--
+-- This program compiles the wordlist files for usage with the “spell” program.
+--
+-- It loads the input file and extracts all words. For this list of words a
+-- 'Data.Trie.skeleton' is created. This 'Trie' gets binary encoded, compressed
+-- and written to the output file.
+--
+-- If the user specifies no output file, the extension “.trie.gz” is appended to
+-- the input filename.
 module Main where
 
 import           Codec.Compression.GZip (compress)
@@ -15,21 +35,29 @@ import           Options.Applicative
 import           System.IO (IOMode (ReadMode), openFile)
 
 
+-- | Represents all command line flags and arguments.
 data CmdLine = CmdLine
     { outputFilename :: Maybe FilePath
     , inputFilename  :: FilePath
     } deriving (Read, Eq, Show)
 
 
+-- | The command line parser.
 cmdLine :: Parser CmdLine
 cmdLine = CmdLine
     <$> optional (strOption $ long "output" <> short 'o' <> metavar "OUTPUT-FILE")
     <*> argument str (metavar "INPUT-FILE")
 
+-- | Calculates the output file. Appends a file extension to input
+-- filename if no output file was given. Otherwise the output filename
+-- is used as is.
 outputFile :: CmdLine -> FilePath
 outputFile (CmdLine Nothing i)  = i ++ ".trie.gz"
 outputFile (CmdLine (Just o) _) = o
 
+-- | Reads the given input file, extracts all words and builds a
+-- 'skeleton'. This skeleton is then encoded as a binary stream,
+-- compressed and written to the output file.
 main:: IO ()
 main = execParser parser >>= \c -> do
   hInput <- openFile (inputFilename c) ReadMode
--- src/Spell.hs
+++ src/Spell.hs
@@ -1,8 +1,31 @@
 {-# LANGUAGE DeriveFunctor     #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE Trustworthy       #-}
+
+-- |
+-- Module      : Main
+-- Description : The spell checkers’ heart.
+-- Copyright   : (c) Stefan Haller, 2014
+--
+-- License     : MIT
+-- Maintainer  : s6171690@mail.zih.tu-dresden.de
+-- Stability   : experimental
+-- Portability : portable
+--
+-- This applications performs spell checking by calculating the minimum edit
+-- distance to each of the words in its wordlist.
+--
+-- The wordlist is a 'Trie' 'Data.Trie.skeleton' which is loaded from a binary
+-- encoded and compressed file. The files can be produced with the
+-- “spell-compile” program.
+--
+-- Only alphabetic characters are checked for corrections. All other characters
+-- are written to the output file without any modifications. The machinery which
+-- allows this are the functions around the 'Annotation' data type.
 module Main where
 
 import           Codec.Compression.GZip (decompress)
+
 import           Control.Applicative
 import           Control.Monad
 
@@ -27,19 +50,29 @@ import           Spell.Edit (Penalties, bestEdits', defaultPenalties)
 import           System.IO
 
 
+-- * Annotations
+
+-- | Annotates a value: Either keep the value or consider it for replacement.
+--
+-- At the type level this is a something like @'Either' a a@.
 data Annotation a = Consider { unAnnotation :: a }
                   | Keep     { unAnnotation :: a }
                   deriving (Functor, Read, Eq, Show)
 
+-- | True if both annotation are of the same type.
 sameAnnotation :: Annotation a -> Annotation a -> Bool
 sameAnnotation (Consider _) (Consider _) = True
 sameAnnotation (Keep     _) (Keep     _) = True
 sameAnnotation           _            _  = False
 
+-- | Returns the function which is able to construct a annotation of the same
+-- type.
 annotationConstr :: Annotation a -> (b -> Annotation b)
 annotationConstr (Consider _) = Consider
 annotationConstr (Keep     _) = Keep
 
+-- | Annotates each 'Char' in the 'String'. All alphabetical characters are
+-- “considered” and the rest is “kept”.
 annotate :: String -> [Annotation Char]
 annotate = map f
   where
@@ -47,6 +80,7 @@ annotate = map f
       | isAlpha x = Consider x
       | otherwise = Keep x
 
+-- | Merges subsequent annotations of the same type.
 mergeAnnotation :: [Annotation Char] -> [Annotation String]
 mergeAnnotation = map f . groupBy sameAnnotation
   where
@@ -54,6 +88,7 @@ mergeAnnotation = map f . groupBy sameAnnotation
     f xs = annotationConstr (head xs) (map unAnnotation xs)
 
 
+-- | Represents all command line flags and arguments.
 data CmdLine = CmdLine
     { wordlistFilename :: FilePath
     , inputFilename    :: FilePath
@@ -63,6 +98,10 @@ data CmdLine = CmdLine
     , cutting          :: Maybe Double
     } deriving (Read, Eq, Show)
 
+
+-- * Command Line Parsing and Processing
+
+-- | The command line parser.
 cmdLine :: Parser CmdLine
 cmdLine = CmdLine
     <$> strOption (short 'w' <> long "wordlist" <> metavar "FILE")
@@ -72,29 +111,41 @@ cmdLine = CmdLine
     <*> switch    (short 'u' <> long "use-confusion")
     <*> optional  (option (short 'c' <> long "cutting" <> metavar "NUMBER"))
 
+-- | Loads the 'Data.Trie.skeleton' from file.
 loadSkeleton :: CmdLine -> IO (Trie Char ())
 loadSkeleton CmdLine { wordlistFilename = f } = do
   content <- B.readFile f
   return . decode $ decompress content
 
+-- | Loads the input file and annotates the 'TS.Text'.
 loadInput :: CmdLine -> IO [Annotation TS.Text]
 loadInput CmdLine { inputFilename = f } = do
   handle <- openFile f ReadMode
   content <- TLI.hGetContents handle
   return $ annotateText content
 
+-- | Returns the penalty functions according to command line flags.
 penalties :: CmdLine -> Penalties Char Double
 penalties CmdLine { useConfusion = c }
   | c         = confusionPenalties
   | otherwise = defaultPenalties
 
+-- * Main Functionality of the Program
+
+-- | Annotates each word in the 'TS.Text'.
 annotateText :: TL.Text -> [Annotation TS.Text]
 annotateText = map (fmap TS.pack) . mergeAnnotation . annotate . TL.unpack
 
+-- | Processes each annotation. All values which should be kept are written to
+-- the output handle as is. All other values are mangled through the function in
+-- the first argument and then written to the handle.
 processAnnotation :: (TS.Text -> IO TS.Text) -> Handle -> Annotation TS.Text -> IO ()
 processAnnotation _ h (Keep t) = TSI.hPutStr h t
 processAnnotation f h (Consider t) = f t >>= TSI.hPutStr h
 
+-- | Determines the replacement for the given reference word. The first argument
+-- specifies if batching mode is enabled. If batching mode is disabled, the user
+-- is asked for every single word.
 determineFunc :: Bool                      -- ^ Batch mode?
                  -> Penalties Char Double  -- ^ Penalties to use for MED.
                  -> Maybe Double           -- ^ Just value determines cut off.
@@ -129,6 +180,9 @@ determineFunc False p c s w = do
       TSI.putStrLn "”\n"
       return new
 
+-- | The main entry point for the application. Parses the command line argument,
+-- loads the wordlist file, loads the input file and then processes each of the
+-- annotated words.
 main :: IO ()
 main = execParser parser >>= \c -> do
   skel <- loadSkeleton c
